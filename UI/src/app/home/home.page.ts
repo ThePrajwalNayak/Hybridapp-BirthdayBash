c6da51dcca3e95c7dd4c22c9808d3abfb8bfe1b8
Prajwal Nayak <theprajwalnayak@gmail.com>
2019-11-10 14:43:06 +0530

    Fixing bug in filter serach

--- UI/src/app/home/home.page.ts
+++ UI/src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router'; 
+import { Router } from '@angular/router';
 
-import { UserService , SearchType} from '../services/user.service';
+import { UserService, SearchType } from '../services/user.service';
 
 
 
@@ -12,38 +12,57 @@ import { UserService , SearchType} from '../services/user.service';
 })
 export class HomePage implements OnInit {
 
-
-  searchTerm : any = '';
+  searchTerm: any = '';
   type: SearchType = SearchType.LoginName;
-  results : any = [];
+  results: any = [];
+  currentSeachFilter = this.type;
 
-  constructor(private userService : UserService, private router : Router) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
 
   }
 
+  //Call API to get result based on SeachTerm and Search Type
   searchChanged(event) {
     console.log(event.detail.value);
-    // Call our service function which returns an Observable
-    if (this.searchTerm) {
-      this.userService.searchData(this.searchTerm, this.type)
-        .subscribe(data => {
-          this.prepareResult(data);
-        }, error => {
-          console.log(error);
-        });
+    //If user using same filter for search then proceed with API call
+    if (this.currentSeachFilter == this.type) {
+      //Call API if searchTerm not empty 
+      if (this.searchTerm) {
+        this.userService.searchData(this.searchTerm, this.type)
+          .subscribe(data => {
+            this.prepareResult(data);
+          }, error => {
+            this.results = [];
+            console.log(error);
+          });
+      }else{
+        this.results = [];
+      }
     }
   }
 
-  prepareResult(data){
+  prepareResult(data) {
     this.results = [];
-    if(this.type == 'Users'){
+    if (this.type == 'Users') {
       this.results.push(data);
-    }else if(this.type == 'repositories'){
-      let objCopy  = JSON.parse(JSON.stringify(data.items));
+    } else if (this.type == 'repositories') {
+      let objCopy = JSON.parse(JSON.stringify(data.items));
       this.results = objCopy;
     }
+  }
+
+  resetSearchTerm(event) {
+    this.searchTerm = '';
+    this.results = [];
+  }
+
+  filterChanged(event) {
+    console.log(event.detail.value);
+    this.currentSeachFilter = event.detail.value;
+    this.searchTerm = '';
+    this.results = [];
   }
 
   goToUserDetails(user) {
@@ -53,6 +72,6 @@ export class HomePage implements OnInit {
 
   avatarLetter(word) {
     return word.charAt(0).toUpperCase();
-}
+  }
 
 }
